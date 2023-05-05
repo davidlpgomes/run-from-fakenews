448212882fe545a0af4c45c3faaf45f971e75c32
David Gomes <david.gomes@tuta.io>
2023-05-05 03:19:21 -0300

    Add position

---
+++ Position.java
@@ -0,0 +1,33 @@
+public class Position {
+    private int x;
+    private int y;
+
+    public Position(int x, int y) {
+        this.setX(x);
+        this.setY(y);
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public void setX(int x) {
+        if (x < 0)
+            return;
+
+        this.x = x;
+        return;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public void setY(int y) {
+        if (y < 0)
+            return;
+
+        this.y = y;
+        return;
+    }
+}
