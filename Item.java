9dcd173351f0f52a173dcd792ab2dfc01245086d
David Gomes <david.gomes@tuta.io>
2023-05-05 03:18:53 -0300

    Add item

---
+++ Item.java
@@ -0,0 +1,28 @@
+public class Item extends Entity {
+    private static final String color = Colors.ANSI_YELLOW;
+
+    private ItemType type;
+
+    public Item(Position position, ItemType type) {
+        this.setPosition(position);
+        this.setType(type);
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public ItemType getType() {
+        return this.type;
+    }
+    
+    public void setType(ItemType type) {
+        this.type = type;
+
+        return;
+    }
+
+    public String toString() {
+        return "??";
+    }
+}
---
+++ ItemType.java
@@ -0,0 +1,6 @@
+public enum ItemType {
+    REPORT_FAKE_NEWS,
+    RUN_AWAY,
+    READ_A_REAL_NEWS,
+    HEAR_A_RUMOR;
+}
