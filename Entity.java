caad6d6a3706aa482a69e2d5cc80ee94c0d99f81
David Gomes <david.gomes@tuta.io>
2023-05-05 03:18:11 -0300

    Add entity

---
+++ Entity.java
@@ -0,0 +1,17 @@
+public abstract class Entity {
+    private Position position;
+
+    public Position getPosition() {
+        return this.position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+
+        return;
+    }
+
+    public abstract String getColor();
+
+    public abstract String toString();
+}
