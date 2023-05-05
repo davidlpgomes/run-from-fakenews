2b0fc58c8071ce49492e307765b2da8dd150e958
David Gomes <david.gomes@tuta.io>
2023-05-05 03:19:39 -0300

    Add main

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("[RFFN] Initializing game...");
+
+        Game game = new Game();
+
+        game.init();
+        game.print();
+
+        return;
+    }
+}
