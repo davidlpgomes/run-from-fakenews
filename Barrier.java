5803fbed82aff3cd564f4826681a20046ac92d17
David Gomes <david.gomes@tuta.io>
2023-05-05 03:18:38 -0300

    Add barrier

---
+++ Barrier.java
@@ -0,0 +1,15 @@
+public class Barrier extends Entity {
+    private static final String color = Colors.ANSI_WHITE;
+
+    public Barrier(Position position) {
+        this.setPosition(position);
+    }
+    
+    public String getColor() {
+        return this.color;
+    }
+
+    public String toString() {
+        return "XX";
+    }
+}
